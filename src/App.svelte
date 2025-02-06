7b101e21ffef58e18ce6ac07410bfc6960a682f2
Lucas Matias Blanco <lucasmatiasblanco@outlook.com>
2025-02-06 18:04:06 -0300

    Added basic crud actions for videos

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
-  import { FolderInput, BadgePlus, ListVideo } from "lucide-svelte";
+  import {
+    FolderInput,
+    BadgePlus,
+    ListVideo,
+    FolderPlus,
+    ChevronLeft,
+  } from "lucide-svelte";
   import { db } from "./background/background";
   import List from "./lib/List.svelte";
+  import { onMount } from "svelte";
+
+  let showExistingFolders = $state(false);
+  let showEmojiOptions = $state(false);
+  let url = $state("");
+  let title = $state("");
+
+  onMount(async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    if (tab?.url) {
+      url = tab.url;
+      title = tab.title || "";
+    }
+  });
 
   const handleSubmit = async (e) => {
-    const formData = new FormData(e.target);
-    const title = formData.get("url") as string;
+    e.preventDefault();
     try {
       await db.videos.add({
         title: title,
         folder_id: 1,
-        url: "https://www.youtube.com/watch?v=1",
+        url: url,
         created_at: new Date(),
       });
     } catch (error) {
@@ -19,16 +41,20 @@
   };
 </script>
 
-<main class="bg-pink-300 p-2 text-black w-[400px]">
+<main class="bg-pink-300 p-2 text-black w-[400px] px-3">
   <h1>guardaditto</h1>
-  <form class="flex gap-2" on:submit|preventDefault={handleSubmit}>
+  <form class="flex gap-2" onsubmit={handleSubmit}>
     <input
+      bind:value={url}
+      readonly
       name="url"
       type="text"
       class="flex h-6 w-full rounded-sm border border-black border-input bg-background px-2 py-2 text-xs ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
     />
     <div class="flex gap-2">
       <button
+        type="button"
+        onclick={() => (showExistingFolders = !showExistingFolders)}
         class="border border-black h-6 border-input bg-background rounded-sm px-3 cursor-pointer hover:bg-accent hover:text-accent-foreground"
         ><FolderInput class="text-black " size={15} /></button
       >
@@ -40,13 +66,49 @@
     </div>
   </form>
   <!-- <div>No folders</div> -->
-  <div class="flex gap-2 p-2 items-center text-sm">
-    <div><ListVideo size={20} /></div>
-    <div class="h-4 bg-black w-[1px]"></div>
-    <div>😍</div>
-    <div>🔥</div>
-    <div>👀</div>
-  </div>
+  {#if !showEmojiOptions}
+    {#if showExistingFolders}
+      <div class="flex gap-2 p-2 items-center text-sm">
+        <div>🐟</div>
+        <div>🐠</div>
+        <div>🐡</div>
+      </div>
+    {:else}
+      <div class="flex gap-2 p-2 items-center text-sm">
+        <div><ListVideo size={20} /></div>
+        <div class="h-4 bg-black w-[1px]"></div>
+        <div>😍</div>
+        <div>🔥</div>
+        <div>👀</div>
+        <div class="h-4 bg-black w-[1px]"></div>
+        <button class="self-end"
+          ><FolderPlus
+            class="text-black "
+            size={15}
+            onclick={() => (showEmojiOptions = true)}
+          /></button
+        >
+      </div>
+    {/if}
+  {:else}
+    <div class="flex gap-2 p-2 items-center text-sm">
+      <button class="self-end"
+        ><ChevronLeft
+          class="text-black "
+          size={15}
+          onclick={() => (showEmojiOptions = false)}
+        /></button
+      >
+      <div class="h-4 bg-black w-[1px]"></div>
+      <div>😍</div>
+      <div>🔥</div>
+      <div>👀</div>
+      <div>😍</div>
+      <div>🔥</div>
+      <div>👀</div>
+    </div>
+  {/if}
+
   <hr />
   <List />
 </main>
--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -2,31 +2,46 @@
   import { liveQuery } from "dexie";
   import { db } from "../background/background";
   import { YoutubeBrands } from "svelte-awesome-icons";
+  import { ExternalLink, Trash2 } from "lucide-svelte";
 
   let videos = liveQuery(() => db.videos.toArray());
+
+  // let arr = $state(Array(5).fill(null));
+  // function addRow() {
+  //   arr.push(null);
+  // }
 </script>
 
-<ul class="flex flex-col h-50 overflow-y-scroll">
+<ul class="flex flex-col h-50 overflow-y-auto pe-3">
   {#if $videos}
     {#each $videos as video (video.id)}
-      <li class="flex items-center gap-2">
-        <YoutubeBrands /> <span>{video.title}</span>
+      <li class="flex items-center gap-2 text-xs">
+        <YoutubeBrands class="basis-auto" />
+        <span class="text-left truncate basis-full">{video.title}</span>
+        <a href={video.url} target="_blank">
+          <ExternalLink class="basis-auto" size={15} /></a
+        >
+        <button
+          class="cursor-pointer"
+          onclick={() => db.videos.delete(video.id)}
+          ><Trash2 class="basis-auto" size={15} /></button
+        >
       </li>
     {/each}
   {/if}
 </ul>
 
-<!-- <div class="flex flex-col h-50 overflow-y-scroll">
-  <div class="flex items-center gap-2">Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-  <div class="flex items-center gap-2"><YoutubeBrands />Videos1</div>
-</div> -->
+<!-- <ul class="flex flex-col h-50 overflow-y-auto pe-3">
+  {#each arr as _, i}
+    <li class="flex items-center gap-2 text-xs">
+      <YoutubeBrands class="basis-auto" />
+      <span class="text-left truncate basis-full">Video Title</span>
+      <a href={""} target="_blank">
+        <ExternalLink class="basis-auto" size={15} /></a
+      >
+      <button class="cursor-pointer"
+        ><Trash2 class="basis-auto" size={15} /></button
+      >
+    </li>
+  {/each}
+</ul> -->

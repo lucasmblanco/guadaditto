f3b14e518b663ac0a4be58374d05cd1e55dd8caa
Lucas Matias Blanco <lucasmatiasblanco@outlook.com>
2025-04-03 18:35:04 -0300

    Major changes in button functionality and placement

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const YOUTUBE_VIDEO_SELECTOR = {
+  homepage: "div.ytd-rich-grid-renderer:nth-child(6)",
+  subscription_page:
+    "ytd-browse.style-scope:nth-child(3) > ytd-two-column-browse-results-renderer:nth-child(11) > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)",
+  channel_videos_page:
+    "ytd-two-column-browse-results-renderer.grid-5-columns > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)",
+  related_videos:
+    "ytd-item-section-renderer.ytd-watch-next-secondary-results-renderer > div:nth-child(3)",
+};
--- src/content/add_button.ts
+++ src/content/add_button.ts
@@ -1,9 +1,11 @@
+import { YOUTUBE_VIDEO_SELECTOR } from "../constants";
+
 function elementMatchesSelectors(element) {
   const selectors = [
-    "div.ytd-rich-grid-renderer:nth-child(6)",
-    "ytd-browse.style-scope:nth-child(3) > ytd-two-column-browse-results-renderer:nth-child(11) > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)",
-    "ytd-two-column-browse-results-renderer.grid-5-columns > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)",
-    "ytd-item-section-renderer.ytd-watch-next-secondary-results-renderer > div:nth-child(3)",
+    YOUTUBE_VIDEO_SELECTOR.homepage,
+    YOUTUBE_VIDEO_SELECTOR.subscription_page,
+    YOUTUBE_VIDEO_SELECTOR.channel_videos_page,
+    YOUTUBE_VIDEO_SELECTOR.related_videos,
   ];
 
   return selectors.some((selector) => {
@@ -19,7 +21,6 @@ function checkAddedNodes(addedNodes) {
   for (const node of addedNodes) {
     if (node.nodeType === Node.ELEMENT_NODE) {
       if (elementMatchesSelectors(node)) {
-        console.log("Elemento encontrado:", node);
         const observer = new MutationObserver(() => {
           createButton(node);
         });
@@ -42,32 +43,6 @@ function startObserving() {
     childList: true,
     subtree: true,
   });
-
-  // También verificar si los elementos ya existen en el DOM
-  // const existingElements = [];
-
-  // try {
-  //   existingElements.push(
-  //     ...document.querySelectorAll("div.ytd-rich-grid-renderer:nth-child(6)"),
-  //   );
-  //   existingElements.push(
-  //     ...document.querySelectorAll(
-  //       "ytd-browse.style-scope:nth-child(3) > ytd-two-column-browse-results-renderer:nth-child(11) > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)",
-  //     ),
-  //   );
-  //   existingElements.push(
-  //     ...document.querySelectorAll(
-  //       "ytd-two-column-browse-results-renderer.grid-5-columns > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)",
-  //     ),
-  //   );
-  // } catch (error) {
-  //   console.error("Error al buscar elementos existentes:", error);
-  // }
-
-  // for (const element of existingElements) {
-  //   console.log("Elemento existente encontrado:", element);
-  //   // Aquí puedes ejecutar la lógica que necesites con los elementos ya existentes
-  // }
 }
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", startObserving);
@@ -75,41 +50,18 @@ if (document.readyState === "loading") {
   startObserving();
 }
 
-// Para detener el observer cuando ya no lo necesites
-// function stopObserving() {
-//   observer.disconnect();
-//   console.log('Observer detenido');
-// }
-
-// Ejemplo de cómo detener el observer después de un tiempo
-// setTimeout(stopObserving, 60000); // Detener después de 1 minuto
-
-//ytd-compact-video-renderer.style-scope:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(2) > ytd-video-meta-block:nth-child(1) > div:nth-child(1) > div:nth-child(2)
-// ytd-browse.style-scope:nth-child(2) > ytd-two-column-browse-results-renderer:nth-child(11) > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6) > ytd-rich-item-renderer:nth-child(3) > div:nth-child(1) > ytd-rich-grid-media:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > ytd-video-meta-block:nth-child(2) > div:nth-child(1) > div:nth-child(2)
-// ytd-browse.style-scope:nth-child(3) > ytd-two-column-browse-results-renderer:nth-child(11) > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6) > ytd-rich-item-renderer:nth-child(7) > div:nth-child(1) > ytd-rich-grid-media:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > ytd-video-meta-block:nth-child(2) > div:nth-child(1) > div:nth-child(2)
 function createButton(target: Element) {
-  // const lists = target.querySelectorAll("#metadata-line");
-  //#dismissible
   const lists = target.querySelectorAll("#dismissible");
 
   lists.forEach((list) => {
     const buttonExists = list.querySelector(".g-button");
-    // const firstEl = list.querySelector("#metadata-line > span:nth-child(3)");
 
     if (buttonExists) {
       console.log("Button already exists, skipping creation.");
       return;
     } else {
-      // const spanContainer = document.createElement("span");
       const button = document.createElement("button");
       const img = document.createElement("img");
-
-      // spanContainer.classList.add(
-      //   // "inline-metadata-item",
-      //   // "style-scope",
-      //   // "ytd-video-meta-block",
-      //   "g-container",
-      // );
 
       button.classList.add("g-button");
 
@@ -118,9 +70,6 @@ function createButton(target: Element) {
       list.style.position = "relative";
       button.appendChild(img);
       list.appendChild(button);
-      // spanContainer.appendChild(button);
-      // list.appendChild(spanContainer);
-      // detailEl?.appendChild(spanContainer);
 
       let videoContainer = list.closest(
         "ytd-rich-grid-media, ytd-video-renderer",
@@ -133,7 +82,6 @@ function createButton(target: Element) {
       if (videoRelatedContainer) {
         videoContainer = videoRelatedContainer;
       }
-      //ytd-compact-video-renderer
 
       const linkElement = videoContainer?.querySelector("a#thumbnail");
       const videoUrl = linkElement
@@ -167,35 +115,3 @@ function createButton(target: Element) {
     }
   });
 }
-
-// function waitForElement(
-//   selector: string,
-//   callback: (element: Element) => void,
-// ) {
-//   // const targets = document.querySelectorAll(selector);
-//   // if (targets.length >= 1) {
-//   //   targets.forEach((target) => callback(target));
-//   // } else {
-
-//   const observer = new MutationObserver((mutations, obs) => {
-//     const elements = document.querySelectorAll(selector);
-//     if (elements.length >= 1) {
-//       elements.forEach((target) => callback(target));
-//     }
-//   });
-//   observer.observe(document.body, { childList: true, subtree: true });
-
-//   // }
-// }
-
-// waitForElement("#contents.ytd-rich-grid-renderer", (target) => {
-//   const observer = new MutationObserver(() => {
-//     createButton(target);
-//   });
-
-//   observer.observe(target, { childList: true, subtree: true });
-// });
-
-// // div.ytd-rich-grid-renderer:nth-child(6)
-// // ytd-browse.style-scope:nth-child(3) > ytd-two-column-browse-results-renderer:nth-child(11) > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)
-// // ytd-two-column-browse-results-renderer.grid-5-columns > div:nth-child(1) > ytd-rich-grid-renderer:nth-child(1) > div:nth-child(6)

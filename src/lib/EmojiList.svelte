45bcd6c9f60f905c4d94c46f574bb8d6848000ec
Lucas Matias Blanco <lucasmatiasblanco@outlook.com>
2025-04-15 10:10:46 -0300

    Some ui fixes in EmojiList component

--- src/lib/EmojiList.svelte
+++ src/lib/EmojiList.svelte
@@ -32,28 +32,29 @@
 </script>
 
 <div class="absolute top-0 left-0 h-full" transition:slide>
-  <div class="grid h-12 w-full grid-cols-11 items-center gap-2 overflow-y-auto">
+  <!-- <div class="grid h-12 w-full grid-cols-11 items-center gap-2 overflow-y-auto"> -->
+  <div class="flex pb-2">
     {#each Object.keys(emojisCat) as category}
       <button
         onclick={() => setActiveCategory(category)}
         class="inline-flex h-10 w-10 items-center {category === activeCategory
-          ? 'bg-primary-ditto'
-          : ''} ring-offset-background focus-visible:ring-ring // hover:bg-primary-ditto justify-center gap-2 rounded-full text-lg font-medium whitespace-nowrap transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+          ? 'bg-primary-ditto !border-accent-ditto'
+          : ''} ring-offset-background focus-visible:ring-ring hover:bg-primary-ditto justify-center gap-2 rounded-full border border-transparent text-lg font-medium whitespace-nowrap transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
       >
         {emojisCat[category][0].emoji}
       </button>
     {/each}
   </div>
   <div
-    class="border-accent-ditto bg-primary-ditto relative h-[calc(100%-3rem)] overflow-y-auto rounded-xl border-1"
+    class="border-accent-ditto bg-primary-ditto relative h-[calc(100%-3rem)] overflow-y-auto rounded-l-xl border-1"
   >
-    <div class=" relative grid grid-cols-10 gap-[.1rem]">
+    <div class="relative grid grid-cols-10 gap-[.1rem] p-2">
       {#if activeCategory}
         {#each emojisCat[activeCategory] as emoji}
           <button
             onclick={createFolder}
             value={emoji.emoji}
-            class="hover: h-8 w-full self-center rounded-full text-center hover:backdrop-saturate-200"
+            class="h-8 w-full self-center rounded-full text-center hover:backdrop-saturate-200"
             >{emoji.emoji}</button
           >
         {/each}
